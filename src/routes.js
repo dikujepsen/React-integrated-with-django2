ce608cc15175da8f3b84e2a77c9edd660b3ba60c
JacobJepsen <jepsenjacob@gmail.com>
2017-07-05 20:57:49 +0200

    add routes

---
+++ src/routes.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jacob on 26-06-17.
+ */
+import React from 'react';
+import {Route, IndexRoute} from 'react-router';
+import App from './components/App';
+
+export default (
+  <Route path="/react/" component={App} />
+);
