bbf843ffe7213577f6686dbaa10a72d928fb655f
JacobJepsen <jepsenjacob@gmail.com>
2017-07-10 16:21:10 +0200

    add readonly fields

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,15 +3,18 @@ from .models import Course, Author
 
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
-    author_name = serializers.CharField(source='author')
-    id = serializers.IntegerField()
+    author_name = serializers.CharField(source='author', read_only=True)
+    id = serializers.ReadOnlyField()
 
     class Meta:
         model = Course
         fields = '__all__'
 
+# fetch('http://localhost:8000/api/courses/3/', {method: "PUT", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: "{'title': 'Architecting Applications for the Real World333'}"});
+
 
 class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
 
     class Meta:
         model = Author
