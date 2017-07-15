fd44ab17f20750f7f5cf57b24c37b2e11241782b
JacobJepsen <jepsenjacob@gmail.com>
2017-07-15 10:07:20 +0200

    add error handling for login

--- courses/serializers.py
+++ courses/serializers.py
@@ -12,7 +12,7 @@ class CourseSerializer(serializers.HyperlinkedModelSerializer):
 
 # fetch('http://localhost:8000/api/courses/3/', {method: "PUT", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: "{'title': 'Architecting Applications for the Real World333'}"});
 #fetch('http://localhost:8000/api-auth/login/', {method: "POST", headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, body: "{username: 'jacob', password: '1234', csrfmiddlewaretoken: 'sPKQdYma7JMukGPbrT3u3rC3zjX8ZVjVZ1Du7MvNO9EKb8GicwWpZTEGv9V050jX'}", credentials: 'same-origin'});
-
+# fetch('http://localhost:8000/api/courses/3/', {method: "PUT", headers: {'Accept': 'application/json', 'Content-Type': 'application/json', 'X-CSRFTOKEN': 'r9YD6KCqA1p4WGChE6fGjrzxtoVWzNC3szvvBoMY2lGwu8M1JTN95sufJwGqRCEi'}, body: {title: 'Architecting Applications for the Real World333'}, credentials: 'same-origin'});
 # var data = new FormData();
 # data.append('username', 'jacob');;
 # data.append('password', '1234');data.append('csrfmiddlewaretoken', 'CNZN3UjPGcCXXpXVEYprK0HZXtRC6N2XPmnFPZZRQJCi8zVgQbDJXqSUwx9ur7bD');fetch('http://localhost:8000/api-auth/login/', {method: "POST", body: data, credentials: 'same-origin'});
--- src/components/login/LoginSection.js
+++ src/components/login/LoginSection.js
@@ -4,6 +4,23 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import LoginForm from './LoginForm';
 import LoginApi from '../../api/loginApi';
+import toastr from 'toastr';
+
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    let cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = jQuery.trim(cookies[i]);
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
 
 class LoginSection extends React.Component {
     constructor(props, context) {
@@ -26,12 +43,22 @@ class LoginSection extends React.Component {
   }
 
   loginUser(event) {
+    this.setState({saving: true});
     event.preventDefault();
     let token = $("[name='csrfmiddlewaretoken']").val();
     let stateLogin = this.state.login;
     stateLogin.csrfmiddlewaretoken = token;
-    LoginApi.LoginUser(stateLogin);
-    this.setState({saving: true});
+    LoginApi.LoginUser(stateLogin).then(response => {
+      let csrftoken = getCookie('csrftoken');
+      $('#container').data('csrftoken', csrftoken);
+      if (response.type === 'opaqueredirect') {
+        toastr.success('Login successful');
+      } else {
+        toastr.error('Login failed');
+      }
+      stateLogin.password = '';
+      this.setState({saving: false, login: stateLogin});
+    });
   }
 
 
